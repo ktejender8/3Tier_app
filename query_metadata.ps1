d529643c6906b5f044d8a0050d499351ddd8bc36
unknown <ktejender8@gmail.com>
2022-11-21 10:48:19 +0000

    metadata_query

---
+++ query_metadata.ps1
@@ -0,0 +1,17 @@
+﻿$respraw=Invoke-WebRequest -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+
+$respraw
+
+$respraw.content
+
+$respraw.Content | ConvertFrom-Json | ConvertTo-Json -Depth 6
+
+$resp=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01"
+
+$respJson = $resp | ConvertTo-Json -Depth 6
+
+$respJson
+
+$resp.network
+
+Write-Output "VM name - $($resp.compute.name), RG - $($resp.compute.resourceGroupName), VM size - $($resp.compute.vmSize)"
